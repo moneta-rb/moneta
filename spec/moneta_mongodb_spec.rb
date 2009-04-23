a26834644c211323bdaed17168c4d8a6aa3f55a3
Adrian Madrid <aemadrid@gmail.com>
2009-04-22 18:48:56 -0600

    Added basic mongodb support. Added basic mongodb support. Removed 'moneta/file' from spec_helper.rb because it was giving me libSystem.dylib failures on my system, though. Not sure why it was happenning to me.

---
+++ spec/moneta_mongodb_spec.rb
@@ -0,0 +1,12 @@
+require File.dirname(__FILE__) + '/spec_helper'
+require "moneta/mongodb"
+
+describe "Moneta::MongoDB" do
+  before(:each) do
+    @native_expires = true
+    @cache = Moneta::MongoDB.new
+    @cache.clear
+  end
+
+  it_should_behave_like "a read/write Moneta cache"
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,6 @@ $:.push File.join(File.dirname(__FILE__), '..', 'lib')
 require "rubygems"
 require "moneta"
 require "moneta/memory"
-require "moneta/file"
+#require "moneta/file"
 
 require File.expand_path(File.join(File.dirname(__FILE__), "shared"))
