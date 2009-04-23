c85852f76e7a66030b4d895786e6e935a5239021
Adrian Madrid <aemadrid@gmail.com>
2009-04-23 17:03:21 -0600

    Added banchmarks script.

--- lib/moneta/memcache.rb
+++ lib/moneta/memcache.rb
@@ -1,50 +1,52 @@
 begin
   require "memcache"
 rescue LoadError
+  require "memcached"
+rescue LoadError
   puts "You need the memcache gem to use the Memcache moneta store"
-  exit  
+  exit
 end
 
-module Moneta  
+module Moneta
   class Memcache
     def initialize(options = {})
       @cache = MemCache.new(options.delete(:server), options)
     end
-    
+
     def key?(key)
       !self[key].nil?
     end
-    
+
     alias has_key? key?
-    
+
     def [](key)
       @cache.get(key)
     end
-    
+
     def []=(key, value)
       store(key, value)
     end
-    
+
     def fetch(key, default)
       self[key] || default
     end
-    
+
     def delete(key)
       value = self[key]
       @cache.delete(key) if value
       value
     end
-    
+
     def store(key, value, options = {})
       args = [key, value, options[:expires_in]].compact
       @cache.set(*args)
     end
-    
+
     def update_key(key, options = {})
       val = self[key]
       self.store(key, val, options)
     end
-    
+
     def clear
       @cache.flush_all
     end
