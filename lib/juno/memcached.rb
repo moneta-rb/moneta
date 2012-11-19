642e0c31862109e0b3b92c1c248b3fc0b4d522b0
Daniel Mendler <mail@daniel-mendler.de>
2012-11-19 22:20:01 +0100

    explicitly require memcached_native and memcached_dalli to avoid problems with autoload in jruby

--- lib/juno/memcached.rb
+++ lib/juno/memcached.rb
@@ -1,7 +1,9 @@
 module Juno
   begin
+    require 'juno/memcached_native'
     Memcached = MemcachedNative
   rescue LoadError
+    require 'juno/memcached_dalli'
     Memcached = MemcachedDalli
   end
 end
