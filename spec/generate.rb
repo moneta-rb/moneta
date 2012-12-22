f215b621f73cb32a342f66d328d58fe9e5c935a1
Daniel Mendler <mail@daniel-mendler.de>
2012-12-22 16:01:42 +0100

    remove invalid spec

--- spec/generate.rb
+++ spec/generate.rb
@@ -79,12 +79,6 @@ end
     :key => %w(Hash String),
     :specs => [:null, :store, :returndifferent, :increment],
   },
-  'simple_memory_with_json_md5_key_serializer' => {
-    :store => :Memory,
-    :options => ':key_serializer => [:json, :md5]',
-    :key => %w(Hash String),
-    :specs => [:null, :store, :returndifferent, :increment],
-  },
   'simple_memory_with_json_value_serializer' => {
     :store => :Memory,
     :options => ':value_serializer => :json',
