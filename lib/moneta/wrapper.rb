b431691dcdbce64c4ed7ece2ca27a61c5cc4243d
Daniel Mendler <mail@daniel-mendler.de>
2012-12-28 14:43:31 +0100

    add wrapper docu
    [ci skip]

--- lib/moneta/wrapper.rb
+++ lib/moneta/wrapper.rb
@@ -2,30 +2,37 @@ module Moneta
   # Wraps the calls to the adapter
   # @api public
   class Wrapper < Proxy
+    # (see Proxy#key?)
     def key?(key, options = {})
       wrap(:key?, key, options) { super }
     end
 
+    # (see Proxy#load)
     def load(key, options = {})
       wrap(:load, key, options) { super }
     end
 
+    # (see Proxy#store)
     def store(key, value, options = {})
       wrap(:store, key, value, options) { super }
     end
 
+    # (see Proxy#delete)
     def delete(key, options = {})
       wrap(:delete, key, options) { super }
     end
 
+    # (see Proxy#increment)
     def increment(key, amount = 1, options = {})
       wrap(:increment, key, amount, options) { super }
     end
 
+    # (see Proxy#clear)
     def clear(options = {})
       wrap(:clear, options) { super }
     end
 
+    # (see Proxy#close)
     def close
       wrap(:close) { super }
     end
