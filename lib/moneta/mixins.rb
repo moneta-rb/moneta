6856d275442d914757e8b8f6e15c0a7b47b5f6da
Daniel Mendler <mail@daniel-mendler.de>
2012-12-28 15:01:24 +0100

    add comments
    [ci skip]

--- lib/moneta/mixins.rb
+++ lib/moneta/mixins.rb
@@ -1,11 +1,18 @@
 module Moneta
   # @api private
   module OptionSupport
+    # Return Moneta store with default options
+    #
+    # @param [Hash] options Options to merge
+    # @return [OptionMerger]
     # @api public
     def with(options)
       OptionMerger.new(self, options)
     end
 
+    # Return Moneta store with default option :raw => true
+    #
+    # @return [OptionMerger]
     # @api public
     def raw
       @raw_store ||=
@@ -16,11 +23,19 @@ module Moneta
         end
     end
 
+    # Return Moneta store with default prefix option
+    #
+    # @param [String] prefix Key prefix
+    # @return [OptionMerger]
     # @api public
     def prefix(prefix)
       with(:prefix => prefix, :except => :clear)
     end
 
+    # Return Moneta store with default expiration time
+    #
+    # @param [Integer] expires Default expiration time
+    # @return [OptionMerger]
     # @api public
     def expires(expires)
       with(:expires => expires, :only => [:store, :increment])
@@ -35,8 +50,8 @@ module Moneta
     # Exists the value with key
     #
     # @param [Object] key
-    # @return [Boolean]
     # @param [Hash] options
+    # @return [Boolean]
     # @api public
     def key?(key, options = {})
       load(key, options) != nil
