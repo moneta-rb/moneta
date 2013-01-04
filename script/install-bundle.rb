7812a4bdb0d58de577d43709f65a681688528bdb
Daniel Mendler <mail@daniel-mendler.de>
2013-01-04 14:30:17 +0100

    install-bundle: use wget -nv

--- script/install-bundle.rb
+++ script/install-bundle.rb
@@ -20,7 +20,7 @@ end
 
 BUNDLE_FILE = "bundle-#{RUBY_VERSION}-#{ruby}.tar.gz"
 
-if cmd("wget -O #{BUNDLE_FILE} http://s3.amazonaws.com/minad-moneta/#{BUNDLE_FILE}")
+if cmd("wget -nv -O #{BUNDLE_FILE} http://s3.amazonaws.com/minad-moneta/#{BUNDLE_FILE}")
   cmd! 'rm -rf .bundle'
   cmd! "tar -xf #{BUNDLE_FILE}"
   cmd! 'bundle install --path .bundle'
