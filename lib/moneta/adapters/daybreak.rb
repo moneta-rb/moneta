d6f4c5845691862c91ca6b8b4a8bf381c1cb15fa
Daniel Mendler <mail@daniel-mendler.de>
2013-01-11 00:42:04 +0100

    use experimental daybreak database
    [ci skip]

--- lib/moneta/adapters/daybreak.rb
+++ lib/moneta/adapters/daybreak.rb
@@ -5,22 +5,27 @@ module Moneta
     # Daybreak backend
     # @api public
     class Daybreak < Memory
-      # Disable serialization, we have `Moneta::Transformer` for that
-      class DB < ::Daybreak::DB
-        def serialize(value) value; end
-        def parse(value) value; end
+      class Serializer < ::Daybreak::Serializer
+        def key_for(key) key; end
+        def dump(value) value; end
+        def load(value) value; end
       end
 
       # @param [Hash] options
       # @option options [String] :file Database file
       def initialize(options = {})
         raise ArgumentError, 'Option :file is required' unless options[:file]
-        @hash = DB.new(options[:file])
+        @hash = ::Daybreak::DB.new(options[:file], :serializer => Serializer)
+      end
+
+      # (see Proxy#increment)
+      def increment(key, amount = 1, options = {})
+        @hash.lock { return super }
       end
 
       # (see Proxy#close)
       def close
-        @hash.close!
+        @hash.close
         nil
       end
     end
