411108e63f81810844f2abd388745204c1df9e7a
Daniel Mendler <mail@daniel-mendler.de>
2013-01-11 04:36:24 +0100

    update daybreak adapter
    [ci skip]

--- lib/moneta/adapters/daybreak.rb
+++ lib/moneta/adapters/daybreak.rb
@@ -5,17 +5,11 @@ module Moneta
     # Daybreak backend
     # @api public
     class Daybreak < Memory
-      class Serializer < ::Daybreak::Serializer
-        def key_for(key) key; end
-        def dump(value) value; end
-        def load(value) value; end
-      end
-
       # @param [Hash] options
       # @option options [String] :file Database file
       def initialize(options = {})
         raise ArgumentError, 'Option :file is required' unless options[:file]
-        @hash = ::Daybreak::DB.new(options[:file], :serializer => Serializer)
+        @hash = ::Daybreak::DB.new(options[:file], :serializer => ::Daybreak::Serializer::None)
       end
 
       # (see Proxy#increment)
