2eff0b9b973daa6db08a826535541cbd59359486
Daniel Mendler <mail@daniel-mendler.de>
2013-01-18 06:45:35 +0100

    prefer dalli over native memcached

--- lib/moneta/adapters/memcached.rb
+++ lib/moneta/adapters/memcached.rb
@@ -1,11 +1,15 @@
 module Moneta
   module Adapters
+    # Prefer Dalli over native Memcached!
+    #
+    # I measure no performance gain over the Dalli backend
+    # using the Moneta backends.
     begin
-      require 'moneta/adapters/memcached/native'
-      Memcached = MemcachedNative
-    rescue LoadError
       require 'moneta/adapters/memcached/dalli'
       Memcached = MemcachedDalli
+    rescue LoadError
+      require 'moneta/adapters/memcached/native'
+      Memcached = MemcachedNative
     end
   end
 end
