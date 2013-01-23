1aee0bd7ad10b29f1522f2d824408fa1fdef6933
Daniel Mendler <mail@daniel-mendler.de>
2013-01-23 01:12:42 +0100

    add comments
    [ci skip]

--- lib/moneta/weak.rb
+++ lib/moneta/weak.rb
@@ -1,8 +1,16 @@
 module Moneta
+  # Adds weak create support to the underlying store
+  #
+  # @note The increment method will not be thread or multi-process safe (this is meant by weak)
+  # @api public
   class WeakCreate < Proxy
     include CreateSupport
   end
 
+  # Adds weak increment support to the underlying store
+  #
+  # @note The increment method will not be thread or multi-process safe (this is meant by weak)
+  # @api public
   class WeakIncrement < Proxy
     include IncrementSupport
   end
