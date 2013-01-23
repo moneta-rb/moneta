8d4bde78ddb2dcf05e8aa75346a871922a8838c1
Daniel Mendler <mail@daniel-mendler.de>
2013-01-23 01:35:16 +0100

    Adapters::Daybreak - add option :sync to #load and #store
    [ci skip]

--- lib/moneta/adapters/daybreak.rb
+++ lib/moneta/adapters/daybreak.rb
@@ -12,6 +12,19 @@ module Moneta
         @hash = ::Daybreak::DB.new(options[:file], :serializer => ::Daybreak::Serializer::None)
       end
 
+      # (see Proxy#load)
+      def load(key, options = {})
+        @hash.load if options[:sync]
+        @hash[key]
+      end
+
+      # (see Proxy#store)
+      def store(key, value, options = {})
+        @hash[key] = value
+        @hash.flush if options[:sync]
+        value
+      end
+
       # (see Proxy#increment)
       def increment(key, amount = 1, options = {})
         @hash.lock { super }
