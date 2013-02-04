c5d66ea84db23ffdc4a9dae121578e5e701be001
Daniel Mendler <mail@daniel-mendler.de>
2013-02-04 20:32:00 +0100

    weak_create has no concurrency support
    [ci skip]

--- spec/moneta/weak_create_spec.rb
+++ spec/moneta/weak_create_spec.rb
@@ -18,7 +18,6 @@ describe_moneta "weak_create" do
   end
 
   include_context 'setup_store'
-  it_should_behave_like 'concurrent_create'
   it_should_behave_like 'create'
   it_should_behave_like 'features'
   it_should_behave_like 'multiprocess'
