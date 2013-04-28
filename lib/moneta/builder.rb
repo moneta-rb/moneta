6dca86f475f793b8375c13ffce9ed129c6b0a1f5
Daniel Mendler <mail@daniel-mendler.de>
2013-04-28 18:56:06 +0200

    builder simplified

--- lib/moneta/builder.rb
+++ lib/moneta/builder.rb
@@ -5,7 +5,7 @@ module Moneta
     # @yieldparam Builder dsl code block
     def initialize(&block)
       raise ArgumentError, 'No block given' unless block_given?
-      @adapter_set = false
+      @adapter = nil
       @proxies = []
       instance_eval(&block)
     end
@@ -15,12 +15,12 @@ module Moneta
     # @return [Object] Generated Moneta proxy stack
     # @api public
     def build
-      adapter = @proxies.first
+      adapter = @adapter
       if Array === adapter
         klass, options, block = adapter
         adapter = klass.new(options, &block)
       end
-      @proxies[1..-1].inject([adapter]) do |stores, proxy|
+      @proxies.inject([adapter]) do |stores, proxy|
         klass, options, block = proxy
         stores << klass.new(stores.last, options, &block)
       end
@@ -32,7 +32,7 @@ module Moneta
     # @param [Hash] options Options hash
     # @api public
     def use(proxy, options = {}, &block)
-      raise "Cannot add another proxy because the adapter is already specified." if @adapter_set
+      raise 'Cannot add another proxy because the adapter is already specified.' if @adapter
       proxy = Moneta.const_get(proxy) if Symbol === proxy
       raise ArgumentError, 'You must give a Class or a Symbol' unless Class === proxy
       @proxies.unshift [proxy, options, block]
@@ -45,19 +45,18 @@ module Moneta
     # @param [Hash] options Options hash
     # @api public
     def adapter(adapter, options = {}, &block)
-      raise "Cannot set the adapter because the adapter is already specified." if @adapter_set
-      case adapter
-      when Symbol
-        use(Adapters.const_get(adapter), options, &block)
-      when Class
-        use(adapter, options, &block)
-      else
-        raise ArgumentError, 'Adapter must be a Moneta store' unless adapter.respond_to?(:load) && adapter.respond_to?(:store)
-        raise ArgumentError, 'No options allowed' unless options.empty?
-        @proxies.unshift adapter
-        nil
-      end
-      @adapter_set = true
+      raise 'Cannot set the adapter because the adapter is already specified.' if @adapter
+      @adapter =
+        case adapter
+        when Symbol
+          [Adapters.const_get(adapter), options, block]
+        when Class
+          [adapter, options, block]
+        else
+          raise ArgumentError, 'Adapter must be a Moneta store' unless adapter.respond_to?(:load) && adapter.respond_to?(:store)
+          raise ArgumentError, 'No options allowed' unless options.empty?
+          adapter
+        end
     end
   end
 end
