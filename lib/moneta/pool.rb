58939b6983393c8faee0d22610980a7595fd6a34
Daniel Mendler <mail@daniel-mendler.de>
2013-06-24 23:21:37 +0200

    make Moneta::Pool thread safe

--- lib/moneta/pool.rb
+++ lib/moneta/pool.rb
@@ -1,3 +1,5 @@
+require 'thread'
+
 module Moneta
   # Creates a pool of stores.
   # Each thread gets its own store.
@@ -14,24 +16,48 @@ module Moneta
   class Pool < Wrapper
     # @param [Moneta store] adapter The underlying store
     # @param [Hash] options
+    # @option options [String] :mutex (::Mutex.new) Mutex object
     def initialize(options = {}, &block)
       super(nil)
+      @mutex = options[:mutex] || ::Mutex.new
       @builder = Builder.new(&block)
-      @pool, @active = [], {}
+      @pool, @all = [], []
+    end
+
+    def close
+      @mutex.synchronize do
+        raise '#close can only when no thread is using the pool' if @all.size != @pool.size
+        @all.each(&:close)
+        @all = @pool = nil
+      end
     end
 
     protected
 
     def adapter
-      @active[Thread.current]
+      Thread.current[thread_id]
+    end
+
+    def thread_id
+      "Moneta::Pool(#{object_id})"
     end
 
     def wrap(*args)
-      @pool << @builder.build.last if @pool.empty?
-      @active[Thread.current] = @pool.pop
+      store = Thread.current[thread_id] = pop
       yield
     ensure
-      @pool << @active.delete(Thread.current)
+      Thread.current[thread_id] = nil
+      @mutex.synchronize { @pool << store }
+    end
+
+    def pop
+      if @mutex.synchronize { @pool.empty? }
+        store = @builder.build.last
+        @mutex.synchronize { @all << store }
+        store
+      else
+        @mutex.synchronize { @pool.pop }
+      end
     end
   end
 end
