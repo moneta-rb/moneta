251ebe18276fa21e8cd657e7bb3b87f8c9dede42
Daniel Mendler <mail@daniel-mendler.de>
2013-09-08 01:39:33 +0200

    lmdb add multiprocess support

--- spec/moneta/adapter_lmdb_with_db_spec.rb
+++ spec/moneta/adapter_lmdb_with_db_spec.rb
@@ -19,6 +19,7 @@ describe_moneta "adapter_lmdb_with_db" do
   it_should_behave_like 'create'
   it_should_behave_like 'features'
   it_should_behave_like 'increment'
+  it_should_behave_like 'multiprocess'
   it_should_behave_like 'null_stringkey_stringvalue'
   it_should_behave_like 'persist_stringkey_stringvalue'
   it_should_behave_like 'returndifferent_stringkey_stringvalue'
--- spec/moneta/simple_lmdb_spec.rb
+++ spec/moneta/simple_lmdb_spec.rb
@@ -21,6 +21,7 @@ describe_moneta "simple_lmdb" do
   it_should_behave_like 'increment'
   it_should_behave_like 'marshallable_key'
   it_should_behave_like 'marshallable_value'
+  it_should_behave_like 'multiprocess'
   it_should_behave_like 'null_objectkey_objectvalue'
   it_should_behave_like 'null_objectkey_stringvalue'
   it_should_behave_like 'null_objectkey_hashvalue'
