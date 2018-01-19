3a233e0f6a2f83aaccdb36ce0fb4517f749672a2
Alastair Pharo <asppsa@gmail.com>
2018-01-19 20:43:18 +1300

    Moneta::Server: Ignore exceptions from already closed clients

--- lib/moneta/server.rb
+++ lib/moneta/server.rb
@@ -35,7 +35,7 @@ module Moneta
         end
       ensure
         File.unlink(@socket) if @socket
-        @ios.each{ |io| io.close }
+        @ios.each{ |io| io.close rescue nil }
       end
     end
 
