29bef52bcda891e4ff158bf75df8dc569858aff3
Alastair Pharo <asppsa@gmail.com>
2018-04-06 19:25:47 +1000

    kyotocabinet: add each_key support
    The kyotocabinet-ruby-reanimated gem seems to have a weird #each_key
    method that yields arrays, so added a special wrapper

--- spec/moneta/adapter_kyotocabinet_spec.rb
+++ spec/moneta/adapter_kyotocabinet_spec.rb
@@ -3,5 +3,5 @@ describe 'adapter_kyotocabinet' do
     Moneta::Adapters::KyotoCabinet.new(file: File.join(tempdir, "adapter_kyotocabinet.kch"))
   end
 
-  moneta_specs ADAPTER_SPECS.without_multiprocess
+  moneta_specs ADAPTER_SPECS.without_multiprocess.with_each_key
 end
