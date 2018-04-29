2b244cb5be16e2516d7ee4b2c8d1f4561933b9c3
Alastair Pharo <asppsa@gmail.com>
2018-04-29 13:42:38 +1000

    ActiveSupport::Cache: Add support for *_multi methods

--- lib/active_support/cache/moneta_store.rb
+++ lib/active_support/cache/moneta_store.rb
@@ -37,11 +37,72 @@ module ActiveSupport
         super(name, options.merge(raw: true))
       end
 
+      # These are the rails 5.2 versions of these methods, which call into the
+      # lower-level read_multi_entries and write_multi_entries methods.  We
+      # define them here only if the superclass versions don't use the *_entries
+      # methods.
+      unless [:read_multi_entries, :write_multi_entries].all? { |m| superclass.private_instance_methods.include? m }
+        def fetch_multi(*names)
+          raise ArgumentError, "Missing block: `Cache#fetch_multi` requires a block." \
+            unless block_given?
+
+          options = names.extract_options!
+          options = merged_options(options)
+
+          instrument :read_multi, names, options do |payload|
+            read_multi_entries(names, options).tap do |results|
+              payload[:hits] = results.keys
+              payload[:super_operation] = :fetch_multi
+
+              writes = {}
+
+              (names - results.keys).each do |name|
+                results[name] = writes[name] = yield(name)
+              end
+
+              write_multi writes, options
+            end
+          end
+        end
+
+        def read_multi(*names)
+          options = names.extract_options!
+          options = merged_options(options)
+
+          instrument :read_multi, names, options do |payload|
+            read_multi_entries(names, options).tap do |results|
+              payload[:hits] = results.keys
+            end
+          end
+        end
+
+        def write_multi(hash, options = nil)
+          options = merged_options(options)
+
+          instrument :write_multi, hash, options do |payload|
+            entries = hash.each_with_object({}) do |(name, value), memo|
+              memo[normalize_key(name, options)] = \
+                Entry.new(value, options.merge(version: normalize_version(name, options)))
+            end
+
+            write_multi_entries entries, options
+          end
+        end
+      end
+
       protected
 
+      def make_entry(value)
+        case value
+        when ActiveSupport::Cache::Entry, nil
+          value
+        else
+          ActiveSupport::Cache::Entry.new(value)
+        end
+      end
+
       def read_entry(key, options)
-        entry = @store.load(key, moneta_options(options))
-        entry && (ActiveSupport::Cache::Entry === entry ? entry : ActiveSupport::Cache::Entry.new(entry))
+        make_entry(@store.load(key, moneta_options(options)))
       end
 
       def write_entry(key, entry, options)
@@ -55,11 +116,37 @@ module ActiveSupport
         true
       end
 
+      def read_multi_entries(names, options)
+        keys = names.map { |name| normalize_key(name, options) }
+        entries = @store.
+          values_at(*keys, **moneta_options(options)).
+          map(&method(:make_entry))
+
+        names.zip(keys, entries).map do |name, key, entry|
+          next if entry.nil?
+          delete_entry(key, options) if entry.expired?
+          next if entry.expired? || entry.mismatched?(normalize_version(name, options))
+
+          [name, entry.value]
+        end.compact.to_h
+      end
+
+      def write_multi_entries(hash, options)
+        pairs = if options[:raw]
+                  hash.transform_values { |entry| entry.value.to_s }
+                else
+                  hash
+                end
+
+        @store.merge!(pairs, moneta_options(options))
+        hash
+      end
+
       private
 
       def moneta_options(options)
         new_options = options ? options.dup : {}
-        new_options[:expires] = new_options.delete(:expires_in).to_i if new_options.include?(:expires_in)
+        new_options[:expires] = new_options.delete(:expires_in).to_r if new_options.include?(:expires_in)
         new_options
       end
     end
