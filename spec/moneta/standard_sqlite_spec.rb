41fd1e892c9f00fb8dbc9723308ec0b7a2e428d8
Alastair Pharo <asppsa@gmail.com>
2018-05-06 18:27:46 +1000

    specs: attempt to fix sqlite_with_expires spec (#149)
    - use WAL journal mode, which is reputedly faster
    - use a higher `min_ttl`

--- spec/moneta/standard_sqlite_spec.rb
+++ spec/moneta/standard_sqlite_spec.rb
@@ -1,6 +1,6 @@
 describe 'standard_sqlite' do
   moneta_store :Sqlite do
-    {file: File.join(tempdir, "simple_sqlite")}
+    {file: File.join(tempdir, "standard_sqlite")}
   end
 
   moneta_specs STANDARD_SPECS.without_concurrent
