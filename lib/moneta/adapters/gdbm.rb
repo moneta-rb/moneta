161bd80653a5ddca7d5c1ee12cf3328fbd2fa51b
Alastair Pharo <asppsa@gmail.com>
2019-02-08 15:08:00 +1100

    gdbm, sdbm: inherit from dbm adapter

--- lib/moneta/adapters/gdbm.rb
+++ lib/moneta/adapters/gdbm.rb
@@ -4,7 +4,7 @@ module Moneta
   module Adapters
     # GDBM backend
     # @api public
-    class GDBM < Memory
+    class GDBM < DBM
       # @param [Hash] options
       # @option options [String] :file Database file
       # @option options [::GDBM] :backend Use existing backend instance
@@ -14,12 +14,6 @@ module Moneta
             raise ArgumentError, 'Option :file is required' unless options[:file]
             ::GDBM.new(options[:file])
           end
-      end
-
-      # (see Proxy#close)
-      def close
-        @backend.close
-        nil
       end
     end
   end
--- lib/moneta/adapters/sdbm.rb
+++ lib/moneta/adapters/sdbm.rb
@@ -4,7 +4,7 @@ module Moneta
   module Adapters
     # SDBM backend
     # @api public
-    class SDBM < Memory
+    class SDBM < DBM
       # @param [Hash] options
       # @option options [String] :file Database file
       # @option options [::SDBM] :backend Use existing backend instance
@@ -14,12 +14,6 @@ module Moneta
             raise ArgumentError, 'Option :file is required' unless options[:file]
             ::SDBM.new(options[:file])
           end
-      end
-
-      # (see Proxy#close)
-      def close
-        @backend.close
-        nil
       end
     end
   end
