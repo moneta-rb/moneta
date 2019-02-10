078d972c88ece34aef7907891afb1594f0e7a98b
Alastair Pharo <asppsa@gmail.com>
2019-02-11 00:33:37 +1100

    memory: use EachKeySupport

--- lib/moneta/adapters/memory.rb
+++ lib/moneta/adapters/memory.rb
@@ -7,31 +7,12 @@ module Moneta
       include HashAdapter
       include IncrementSupport
       include CreateSupport
-
-      supports :each_key
+      include EachKeySupport
 
       # @param [Hash] options Options hash
       # @option options [Hash] :backend Use existing backend instance
       def initialize(options = {})
         @backend = options[:backend] || {}
-      end
-
-      def each_key(&block)
-        return enum_for(:each_key) unless block_given?
-
-        if @backend.respond_to?(:each_key)
-          @backend.each_key(&block)
-        elsif @backend.respond_to?(:keys)
-          if keys = @backend.keys
-            keys.each(&block)
-          end
-        elsif @backend.respond_to?(:each)
-          @backend.each { |k| yield(k) }
-        else
-          raise ::NotImplementedError, "No enumerator found on backend"
-        end
-
-        self
       end
     end
   end
