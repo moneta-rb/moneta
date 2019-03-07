5addc2d3e7d0090099da2338c3b28b757e75fe93
Alastair Pharo <asppsa@gmail.com>
2019-03-07 22:40:08 +1100

    KyotoCabinet: Implement atomic increment and "multi" methods

--- lib/moneta/adapters/kyotocabinet.rb
+++ lib/moneta/adapters/kyotocabinet.rb
@@ -7,10 +7,8 @@ module Moneta
     class KyotoCabinet
       include Defaults
       include HashAdapter
-      include IncrementSupport
-      include CreateSupport
 
-      supports :each_key
+      supports :each_key, :increment, :create
 
       # @param [Hash] options
       # @option options [String] :file Database file
@@ -49,8 +47,61 @@ module Moneta
 
       # (see Proxy#each_key)
       def each_key
-        return enum_for(:each_key) unless block_given?
-        @backend.each_key{ |arr| yield arr.first }
+        return enum_for(:each_key) { @backend.count } unless block_given?
+        @backend.each_key{ |arr| yield arr[0] }
+        self
+      end
+
+      # (see Proxy#increment)
+      def increment(key, amount = 1, options = {})
+        ret = nil
+        success = @backend.accept(key) do |key, value|
+          if value
+            ret = Integer(value) + amount
+          else
+            ret = amount
+          end
+          ret.to_s
+        end
+
+        raise @backend.error unless success
+        ret
+      end
+
+      # (see Proxy#slice)
+      def slice(*keys, atomic: true, **options)
+        @backend.get_bulk(keys, atomic)
+      end
+
+      # (see Proxy#values_at)
+      def values_at(*keys, **options)
+        hash = slice(*keys, **options)
+        keys.map { |key| hash[key] }
+      end
+
+      # (see Proxy#merge!)
+      def merge!(pairs, options = {})
+        hard = options.key?(:hard) ? options[:hard] : false
+        atomic = options.key?(:atomic) ? options[:atomic] : true
+
+        success =
+          if block_given?
+            @backend.transaction(hard) do
+              existing = slice(*pairs.map { |k, _| k }, **options)
+              pairs = pairs.map do |key, new_value|
+                if existing.key?(key)
+                  [key, yield(key, existing[key], new_value)]
+                else
+                  [key, new_value]
+                end
+              end
+              @backend.set_bulk(pairs.to_h, atomic) >= 0
+            end
+          else
+            @backend.set_bulk(pairs.to_h, atomic) >= 0
+          end
+
+        raise @backend.error unless success
         self
       end
     end
