b62c9da8686b7de1b62021c8204d73b4bda4115b
Alastair Pharo <asppsa@gmail.com>
2019-03-14 14:57:43 +1100

    Docs: revert some changes to feature matrix
    The stylesheet setup doesn't work on rubydoc.info, so switch back to using style
    attributes.
    
    [ci skip]

--- yard/config.rb
+++
@@ -1,1 +0,0 @@
-YARD::Templates::Engine.register_template_path File.join(__dir__, 'templates')
--- yard/templates/default/layout/html/setup.rb
+++
@@ -1,4 +0,0 @@
-def stylesheets
-  # Load the existing stylesheets while appending the custom one
-  super + %w(css/custom.css)
-end
