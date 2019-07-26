7ac81ad47cb467b74a18688a66ea4064f06d8da6
Alastair Pharo <me@asph.dev>
2019-07-26 16:35:34 +1000

    Specs: loosen requirements around raw storage of inc/dec numbers
    Allow for the field to be padded by whitespace.  This is what Memcached
    does

--- spec/features/increment.rb
+++ spec/features/increment.rb
@@ -1,72 +1,72 @@
 shared_examples :increment do
   it 'initializes in #increment with 1' do
-    store.key?('inckey').should be false
-    store.increment('inckey').should == 1
-    store.key?('inckey').should be true
-    store.raw['inckey'].should == '1'
-    store.raw.load('inckey').should == '1'
-    store.load('inckey', raw: true).should == '1'
+    expect(store.key?('inckey')).to be false
+    expect(store.increment('inckey')).to eq 1
+    expect(store.key?('inckey')).to be true
+    expect(store.raw['inckey']).to match(/^1\b/)
+    expect(store.raw.load('inckey')).to match(/^1\b/)
+    expect(store.load('inckey', raw: true)).to match(/^1\b/)
 
-    store.delete('inckey', raw: true).should == '1'
-    store.key?('inckey').should be false
+    expect(store.delete('inckey', raw: true)).to match(/^1\b/)
+    expect(store.key?('inckey')).to be false
   end
 
   it 'initializes in #increment with higher value' do
-    store.increment('inckey', 42).should == 42
-    store.key?('inckey').should be true
-    store.raw['inckey'].should == '42'
-    store.delete('inckey', raw: true).should == '42'
+    expect(store.increment('inckey', 42)).to eq 42
+    expect(store.key?('inckey')).to be true
+    expect(store.raw['inckey']).to match(/^42\b/)
+    expect(store.delete('inckey', raw: true)).to match(/^42\b/)
   end
 
   it 'initializes in #increment with 0' do
-    store.increment('inckey', 0).should == 0
-    store.key?('inckey').should be true
-    store.raw['inckey'].should == '0'
-    store.delete('inckey', raw: true).should == '0'
+    expect(store.increment('inckey', 0)).to eq 0
+    expect(store.key?('inckey')).to be true
+    expect(store.raw['inckey']).to match(/^0\b/)
+    expect(store.delete('inckey', raw: true)).to match(/^0\b/)
   end
 
   it 'initializes in #decrement with 0' do
-    store.decrement('inckey', 0).should == 0
-    store.raw['inckey'].should == '0'
+    expect(store.decrement('inckey', 0)).to eq 0
+    expect(store.raw['inckey']).to match(/^0\b/)
   end
 
   it 'initializes in #decrement with negative value' do
-    store.decrement('inckey', -42).should == 42
-    store.raw['inckey'].should == '42'
+    expect(store.decrement('inckey', -42)).to eq 42
+    expect(store.raw['inckey']).to match(/^42\b/)
   end
 
   it 'supports incrementing existing value by value' do
-    store.increment('inckey').should == 1
-    store.increment('inckey', 42).should == 43
-    store.raw['inckey'].should == '43'
+    expect(store.increment('inckey')).to eq 1
+    expect(store.increment('inckey', 42)).to eq 43
+    expect(store.raw['inckey']).to match(/^43\b/)
   end
 
   it 'supports decrementing existing value by value' do
-    store.increment('inckey').should == 1
-    store.decrement('inckey').should == 0
-    store.increment('inckey', 42).should == 42
-    store.decrement('inckey', 2).should == 40
-    store.raw['inckey'].should == '40'
+    expect(store.increment('inckey')).to eq 1
+    expect(store.decrement('inckey')).to eq 0
+    expect(store.increment('inckey', 42)).to eq 42
+    expect(store.decrement('inckey', 2)).to eq 40
+    expect(store.raw['inckey']).to match(/^40\b/)
   end
 
   it 'supports incrementing existing value by 0' do
-    store.increment('inckey').should == 1
-    store.increment('inckey', 0).should == 1
-    store.raw['inckey'].should == '1'
+    expect(store.increment('inckey')).to eq 1
+    expect(store.increment('inckey', 0)).to eq 1
+    expect(store.raw['inckey']).to match(/^1\b/)
   end
 
   it 'supports decrementing existing value' do
-    store.increment('inckey', 10).should == 10
-    store.increment('inckey', -5).should == 5
-    store.raw['inckey'].should == '5'
-    store.increment('inckey', -5).should == 0
-    store.raw['inckey'].should == '0'
+    expect(store.increment('inckey', 10)).to eq 10
+    expect(store.increment('inckey', -5)).to eq 5
+    expect(store.raw['inckey']).to match(/^5\b/)
+    expect(store.increment('inckey', -5)).to eq 0
+    expect(store.raw['inckey']).to match(/^0\b/)
   end
 
   it 'interprets raw value as integer' do
     store.store('inckey', '42', raw: true)
-    store.increment('inckey').should == 43
-    store.raw['inckey'].should == '43'
+    expect(store.increment('inckey')).to eq 43
+    expect(store.raw['inckey']).to match(/^43\b/)
   end
 
   it 'raises error in #increment on non integer value' do
@@ -88,10 +88,10 @@ shared_examples :increment do
     b = Moneta::Semaphore.new(store, 'semaphore', 2)
     c = Moneta::Semaphore.new(store, 'semaphore', 2)
     a.synchronize do
-      a.locked?.should be true
+      expect(a.locked?).to be true
       b.synchronize do
-        b.locked?.should be true
-        c.try_lock.should be false
+        expect(b.locked?).to be true
+        expect(c.try_lock).to be false
       end
     end
   end
