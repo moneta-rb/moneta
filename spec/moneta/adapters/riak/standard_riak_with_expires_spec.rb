62138e8884085a0da6296a067d8774fe66af4e47
Alastair Pharo <me@asph.dev>
2019-07-28 01:42:49 +1000

    Specs: add t_res to riak expiry tests

--- spec/moneta/adapters/riak/standard_riak_with_expires_spec.rb
+++ spec/moneta/adapters/riak/standard_riak_with_expires_spec.rb
@@ -5,6 +5,10 @@ describe 'standard_riak_with_expires', unstable: true, adapter: :Riak do
     Riak.disable_list_keys_warnings = true
   end
 
+  let(:t_res) { 0.125 }
+  let(:min_ttl) { t_res }
+  use_timecop
+
   moneta_store :Riak, {bucket: 'standard_riak_with_expires', expires: true}
   moneta_specs STANDARD_SPECS.without_increment.with_expires.without_create
 end
