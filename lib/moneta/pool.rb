d3614b235078172d9672c9271b437aee5c3cfa57
Alastair Pharo <me@asph.dev>
2019-08-02 18:48:04 +1000

    Pool: rewrite to enable limited pool size and gradual shrinking
    The new implementation runs a manager in a separate thread which is
    responsible for creating and closing stores, and for maintaining a
    waitlist for the available stores.

--- lib/moneta/pool.rb
+++ lib/moneta/pool.rb
@@ -1,67 +1,281 @@
+require 'set'
+
 module Moneta
-  # Creates a pool of stores.
-  # Each thread gets its own store.
+  # Creates a thread-safe pool.  Stores are in the pool are transparently
+  # checked in and out in order to perform operations.
+  #
+  # A `max` setting can be specified in order to limit the pool size.  If `max`
+  # stores are all checked out at once, the next check-out will block until one
+  # of the other stores are checked in.
+  #
+  # A `ttl` setting can be specified, giving the number of seconds to
+  # wait without any activity before shrinking the pool size back down to the
+  # min size.
+  #
+  # A `timeout` setting can be specified, giving the number of seconds to wait
+  # when checking out a store, before an error is raised.  When the pool has a
+  # `:max` size, a timeout is highly advisable.
   #
   # @example Add `Moneta::Pool` to proxy stack
   #   Moneta.build do
   #     use(:Pool) do
-  #       # Every thread gets its own instance
   #       adapter :MemcachedNative
+  #     end
+  #   end
+  #
+  # @example Add `Moneta::Pool` that contains at least 2 stores, and closes any extras after 60 seconds of inactivity
+  #   Moneta.build do
+  #     use(:Pool, min: 2, ttl: 60) do
+  #       adapter :Sqlite, file: 'test.db'
+  #     end
+  #   end
+  #
+  # @example Add `Moneta::Pool` with a max of 10 stores, and a timeout of 5 seconds for checkout
+  #   Moneta.build do
+  #     use(:Pool, max: 10, timeout: 5) do
+  #       adapter :Sqlite, file: 'test.db'
   #     end
   #   end
   #
   # @api public
   class Pool < Wrapper
-    # @param [Hash] options
-    # @option options [String] :mutex (::Mutex.new) Mutex object
-    def initialize(options = {}, &block)
-      super(nil)
-      @mutex = options[:mutex] || ::Mutex.new
-      @id = "Moneta::Pool(#{object_id})"
-      @builder = Builder.new(&block)
-      @pool, @all = [], []
+    # @api private
+    class ShutdownError < ::RuntimeError; end
+    class TimeoutError < ::RuntimeError; end
+
+    # @api private
+    class PoolManager
+      def initialize(builder, min: 0, max: nil, ttl: nil, timeout: nil)
+        @builder = builder
+        @min = min
+        @max = max
+        @ttl = ttl
+        @timeout = timeout
+
+        @inbox = []
+        @mutex = ::Mutex.new
+        @resource = ::ConditionVariable.new
+
+        @stores = Set.new
+        @available = []
+        @waiting = []
+        @waiting_since = [] if @timeout
+        @last_checkout = nil
+        @stopping = false
+
+        # Launch the manager thread
+        run
+      end
+
+      def stats
+        push(:stats, reply: true)
+      end
+
+      def stop
+        push(:stop)
+        sleep 0.1 while @thread.alive?
+        nil
+      end
+
+      def kill!
+        @thread.kill
+        nil
+      end
+
+      def check_out
+        store = push(:check_out, reply: true)
+        raise store if store.is_a? ::Exception
+        store
+      end
+
+      def check_in(store)
+        push(:check_in, store)
+      end
+
+      private
+
+      def run
+        @thread = Thread.new do
+          # Initialize the store
+          @min.times { @available.push(add_store) }
+
+          loop do
+            # Time to wait before there will be stores that should be closed
+            ttl = if @ttl && @last_checkout && !@available.empty?
+                    [@ttl - (Time.now - @last_checkout), 0].max
+                  end
+
+            # Time to wait
+            timeout = if @timeout && !@waiting_since.empty?
+                        longest_waiting = @waiting_since.first
+                        [@timeout - (Time.now - longest_waiting), 0].max
+                      end
+
+            # Block until a message arrives, or until we time out for some reason
+            wait = [ttl, timeout].compact.min
+            if tuple = pop(wait)
+              handle(tuple)
+            end
+
+            # If there are checkout requests that have been waiting too long,
+            # feed them timeout errors.
+            while @timeout && !@waiting.empty? && (Time.now - @waiting_since.first) >= @timeout
+              waiting_since = @waiting_since.shift
+              @waiting.shift.push(TimeoutError.new("Waited %<secs>f seconds" % { secs: Time.now - waiting_since }))
+            end
+
+            # If the last checkout was more than timeout ago, drop any available stores
+            if @stopping || (@ttl && @last_checkout && Time.now - @last_checkout >= @ttl)
+              while (@stopping || @stores.length > @min) and store = @available.pop
+                store.close rescue nil
+                @stores.delete(store)
+              end
+            end
+
+            # Exit the loop if we are done
+            break if @stopping && @stores.empty?
+          end
+        end
+      end
+
+      def push(message, what = nil, reply: false)
+        raise ShutdownError, "Pool has been shutdown" if reply && !@thread.alive?
+        queue = reply ? Queue.new : nil
+        @mutex.synchronize do
+          @inbox.push([message, what, queue])
+          @resource.signal
+        end
+        queue.pop if queue
+      end
+
+      def pop(timeout = nil)
+        @mutex.synchronize do
+          @resource.wait(@mutex, timeout) if @inbox.empty?
+          @inbox.shift
+        end
+      end
+
+      def add_store
+        store = @builder.build.last
+        @stores.add(store)
+        store
+      end
+
+      def handle_check_out(queue)
+        @last_checkout = Time.now
+        if @stopping
+          queue.push(ShutdownError.new("Shutting down"))
+        elsif !@available.empty?
+          queue.push(@available.pop)
+        elsif !@max || @stores.length < @max
+          begin
+            store = add_store
+            queue.push(store)
+          rescue => e
+            queue.push(e)
+          end
+        else
+          @waiting.push(queue)
+          @waiting_since.push(Time.now) if @timeout
+        end
+      end
+
+      def handle_stop
+        @stopping = true
+        # Reject anyone left waiting
+        while queue = @waiting.shift
+          queue.push(ShutdownError.new("Shutting down"))
+        end
+        @waiting_since = [] if @timeout
+      end
+
+      def handle_check_in(store)
+        if !@waiting.empty?
+          @waiting.shift.push(store)
+          @waiting_since.shift if @timeout
+        else
+          @available.push(store)
+        end
+      end
+
+      def handle_stats(reply)
+        reply.push(stores: @stores.length,
+                   available: @available.length,
+                   waiting: @waiting.length,
+                   longest_wait: @timeout && !@waiting_since.empty? ? @waiting_since.first.dup : nil,
+                   stopping: @stopping,
+                   last_checkout: @last_checkout && @last_checkout.dup)
+      end
+
+      def handle(request)
+        cmd, what, reply = request
+        case cmd
+        when :check_out
+          handle_check_out(reply)
+        when :check_in
+          # A checkin request
+          handle_check_in(what)
+        when :stats
+          handle_stats(reply)
+        when :stop
+          # Graceful exit
+          handle_stop
+        end
+      end
     end
 
-    def close
-      @mutex.synchronize do
-        raise '#close can only be called when no thread is using the pool' if @all.size != @pool.size
-        @all.each(&:close)
-        @all = @pool = nil
-      end
+    # @param [Hash] options
+    # @option options [Integer] :min (0) The minimum pool size
+    # @option options [Integer] :max The maximum pool size.  If not specified,
+    #   there is no maximum.
+    # @option options [Numeric] :ttl The number of seconds to keep
+    #   stores above the minumum number around for without activity.  If
+    #   not specified, stores will never be removed.
+    # @option options [Numeric] :timeout The number of seconds to wait for a
+    #   store to become available.  If not specified, will wait forever.
+    # @yield A builder context for speciying how to construct stores
+    def initialize(options = {}, &block)
+      super(nil)
+      @id = "Moneta::Pool(#{object_id})"
+      @manager = PoolManager.new(Builder.new(&block), **options)
+    end
+
+    # Closing has no effect on the pool, as stores are closed in the background
+    # by the manager after the ttl
+    def close; end
+
+    # Tells the manager to close all stores.  It will not be possible to use
+    # the store after this.
+    def stop
+      @manager.stop
+      nil
     end
 
     protected
 
     def adapter
-      Thread.current[@id]
+      Thread.current.thread_variable_get(@id)
+    end
+
+    def adapter=(store)
+      Thread.current.thread_variable_set(@id, store)
     end
 
     def wrap(*args, &block)
-      if checked_out?
+      if adapter
         yield
       else
         check_out!(&block)
       end
     end
 
-    def checked_out?
-      Thread.current.key?(@id) && Thread.current[@id] != nil
-    end
-
     def check_out!
-      store = Thread.current[@id] = pop
+      store = @manager.check_out
+      self.adapter = store
       yield
     ensure
-      Thread.current[@id] = nil
-      @mutex.synchronize { @pool << store }
-    end
-
-    def pop
-      unless store = @mutex.synchronize { @pool.pop }
-        store = @builder.build.last
-        @mutex.synchronize { @all << store }
-      end
-      store
+      self.adapter = nil
+      @manager.check_in store if store
     end
   end
 end
--- spec/moneta/proxies/pool/pool_spec.rb
+++ spec/moneta/proxies/pool/pool_spec.rb
@@ -1,12 +1,297 @@
+require 'timeout'
+
 describe "pool", proxy: :Pool do
-  moneta_build do
-    tempdir = self.tempdir
-    Moneta.build do
-      use :Pool do
-        adapter :File, dir: File.join(tempdir, "pool")
+  describe "Moneta::Pool" do
+    # To test the pool, we create the store once and pass the same object around
+    # whenever a new store is requested.
+    before :context do
+      tempdir = @pool_tempdir = Dir.mktmpdir
+      @pool_store = Moneta.build do
+        use :Pool, max: 2, timeout: 5 do
+          adapter :File, dir: tempdir
+        end
       end
+    end
+
+    # Tell the manager to close all open stores
+    after :context do
+      @pool_store.stop
+      FileUtils.remove_dir(@pool_tempdir)
+    end
+
+    moneta_build { @pool_store }
+    moneta_specs ADAPTER_SPECS.with_each_key
+
+    it "raises an error on check-out the builder fails" do
+      store = Moneta.build do
+        use :Pool do
+          adapter(Class.new do
+            def initialize(options = {})
+              raise "boom"
+            end
+          end)
+        end
+      end
+
+      expect { store['x'] }.to raise_error "boom"
     end
   end
 
-  moneta_specs ADAPTER_SPECS.with_each_key
+  describe "Moneta::Pool::PoolManager" do
+    let :builder do
+      double('builder').tap do |builder|
+        i = -1
+        allow(builder).to receive(:build) do
+          [stores[i+=1]]
+        end
+      end
+    end
+
+    let(:stores) { (0...num).map { |i| double("store#{i}") } }
+
+    after { subject.kill! }
+
+    shared_examples :no_ttl do
+      context "with one store" do
+        let(:num) { 1 }
+
+        it "never expires the store" do
+          store = stores.first
+          expect(builder).to receive(:build).once
+          expect(subject.check_out).to be store
+          expect(subject.stats).to include(stores: 1, available: 0)
+          expect(subject.check_in(store)).to eq nil
+          expect(subject.stats).to include(stores: 1, available: 1)
+          sleep 1
+          expect(subject.stats).to include(stores: 1, available: 1)
+          expect(subject.check_out).to be store
+          expect(subject.check_in(store)).to eq nil
+        end
+      end
+    end
+
+    shared_examples :no_max do
+      context "with 1,000 stores" do
+        let(:num) { 1_000 }
+
+        it "never blocks" do
+          # Check out 1000 stores in 1000 threads
+          threads = (0...num).map do
+            Thread.new { subject.check_out }
+          end
+          expect(threads.map(&:value)).to contain_exactly(*stores)
+          expect(subject.stats).to include(stores: num, available: 0, waiting: 0)
+
+          # Check in the first 50
+          expect(stores.take(50).map { |store| subject.check_in(store) }).to all(be nil)
+          expect(subject.stats).to include(stores: num, available: 50, waiting: 0)
+
+          # Now check those 50 out again
+          threads = (0...50).map do
+            Thread.new { subject.check_out }
+          end
+          expect(threads.map(&:value)).to contain_exactly(*stores.take(50))
+
+          # Finally check in all stores
+          expect(stores.map { |store| subject.check_in(store) }).to all(be nil)
+        end
+      end
+    end
+
+    shared_examples :min do |min|
+      context "with #{min} stores" do
+        let(:num) { min }
+
+        it "starts with #{min} available stores" do
+          expect(subject.stats).to include(stores: min, available: min)
+          expect((0...min).map { subject.check_out }).to contain_exactly(*stores)
+        end
+      end
+    end
+
+    shared_examples :max do |max|
+      context "with #{max} stores" do
+        let(:num) { max }
+
+        after do
+          expect(stores.map { |store| subject.check_in(store) }).to all be_nil
+        end
+
+        it "blocks after #{max} stores have been created" do
+          expect(max.times.map { subject.check_out }).to contain_exactly(*stores)
+          threads = max.times.map { Thread.new { subject.check_out } }
+          Timeout.timeout(5) { sleep 0.1 until subject.stats[:waiting] == max }
+          expect(threads).to all be_alive
+          expect(stores.drop(1).map { |store| subject.check_in(store) }).to all be_nil
+          Timeout.timeout(5) { sleep 0.1 until threads.any? { |t| !t.alive? } }
+          expect(subject.stats).to include(waiting: 1)
+          alive, dead = threads.partition(&:alive?)
+          expect(dead.map(&:value)).to contain_exactly(*stores.drop(1))
+          expect(subject.check_in(stores.first)).to eq nil
+          Timeout.timeout(5) { sleep 0.1 while alive.first.alive? }
+          expect(subject.stats).to include(waiting: 0)
+          expect(alive.first.value).to be stores.first
+        end
+      end
+    end
+
+    shared_examples :ttl do |ttl, min: 0, max: nil|
+      context "with #{ max || min + 10} stores" do
+        let(:num) { max || min + 10 }
+
+        it "closes available stores after ttl" do
+          stores.each do |store|
+            allow(store).to receive(:close)
+          end
+
+          Timeout.timeout(5) { sleep 0.1 until subject.stats[:stores] == min }
+          expect(stores.length.times.map { subject.check_out }).to contain_exactly(*stores)
+          expect(subject.stats).to include(stores: num, available: 0)
+          expect(stores.map { |store| subject.check_in(store) }).to all be_nil
+          expect(subject.stats).to include(stores: num, available: num)
+          sleep ttl
+          expect(subject.stats).to include(stores: min, available: min)
+        end
+      end
+    end
+
+    shared_examples :timeout do |timeout, max:|
+      context "with #{max} stores" do
+        let(:num) { max }
+
+        it "raises a timeout error after waiting too long" do
+          expect((0...num).map { subject.check_out }).to contain_exactly(*stores)
+          # One extra checkout request in a separate thread
+          t = Thread.new do
+            Thread.current.report_on_exception = false if Thread.current.respond_to? :report_on_exception
+            subject.check_out
+          end
+          Timeout.timeout(timeout) { sleep(timeout / 8) until subject.stats[:waiting] == 1 }
+          expect(subject.stats[:longest_wait]).to be_a Time
+          expect(t).to be_alive
+          sleep timeout
+          Timeout.timeout(timeout) { sleep(timeout / 8) while t.alive? }
+          expect { t.value }.to raise_error Moneta::Pool::TimeoutError
+          expect(subject.stats).to include(waiting: 0, longest_wait: nil)
+          expect(stores.map { |store| subject.check_in store }).to all be_nil
+        end
+      end
+    end
+
+    context "with default arguments" do
+      subject { Moneta::Pool::PoolManager.new(builder) }
+      after { subject.kill! }
+
+      include_examples :no_ttl
+      include_examples :no_max
+      include_examples :min, 0
+    end
+
+    context "with max: 10, timeout: 3" do
+      subject { Moneta::Pool::PoolManager.new(builder, max: 10, timeout: 3) }
+      after { subject.kill! }
+
+      include_examples :no_ttl
+      include_examples :max, 10
+      include_examples :min, 0
+      include_examples :timeout, 3, max: 10
+    end
+
+    context "with min: 10" do
+      subject { Moneta::Pool::PoolManager.new(builder, min: 10) }
+      after { subject.kill! }
+
+      include_examples :no_max
+      include_examples :min, 10
+    end
+
+    context "with ttl: 1" do
+      subject { Moneta::Pool::PoolManager.new(builder, ttl: 1) }
+      after { subject.kill! }
+
+      include_examples :ttl, 1, min: 0
+    end
+
+    context "with min: 10, max: 20, ttl: 1, timeout: 3" do
+      subject { Moneta::Pool::PoolManager.new(builder, min: 10, max: 20, ttl: 1, timeout: 3) }
+      after { subject.kill! }
+
+      include_examples :min, 10
+      include_examples :max, 20
+      include_examples :ttl, 1, min: 10, max: 20
+      include_examples :timeout, 3, max: 20
+    end
+
+    context "with min: 10, max: 10, ttl: 2, timeout: 4" do
+      subject { Moneta::Pool::PoolManager.new(builder, min: 10, max: 10, ttl: 2, timeout: 4) }
+      after { subject.kill! }
+
+      include_examples :min, 10
+      include_examples :max, 10
+      include_examples :ttl, 2, min: 10, max: 10
+      include_examples :timeout, 4, max: 10
+    end
+
+    describe '#check_out' do
+      subject { Moneta::Pool::PoolManager.new(builder, max: 1, timeout: 2) }
+      after { subject.kill! }
+
+      let(:num) { 1 }
+
+      it 'yields the store to requesters first come, first served' do
+        store = stores.first
+        procs = (0...10).map { |i| double("proc#{i}") }
+        procs.each do |p|
+          expect(p).to receive(:call).with(store).ordered
+        end
+
+        # Give each thread a chance to issue the checkout request in the right order.
+        threads = procs.map do |p|
+          Thread.new { p.call(subject.check_out) }.tap { sleep 0.1 }
+        end
+
+        # The first thread should return immediately
+        threads.first.join
+
+        # The remaining threads should be waiting for the store to be checked back in
+        expect(threads.drop(1)).to all be_alive
+        expect(subject.stats).to include(waiting: 9)
+
+        threads.each do |t|
+          t.join
+          subject.check_in(store)
+        end
+      end
+
+      it "raises a ShutdownError if the pool is stopped while waiting for a store" do
+        # Exaust the pool
+        store = stores.first
+        allow(store).to receive(:close).once
+        expect(subject.check_out).to eq store
+
+        # Simulate a new thread requesting a check-out
+        t1 = Thread.new do
+          Thread.current.report_on_exception = false if Thread.current.respond_to? :report_on_exception
+          subject.check_out
+        end
+        Timeout.timeout(5) { sleep 0.1 until subject.stats[:waiting] > 0 }
+        expect(t1).to be_alive
+
+        # Meanwhile in another thread, the pool is stopped.
+        t2 = Thread.new { subject.stop }
+
+        # The requesting thread should error out immediately
+        expect { t1.value }.to raise_error Moneta::Pool::ShutdownError
+
+        # In this thread we return the store to the pool, allowing graceful shutdown to complete.
+        subject.check_in(store)
+        expect(t2.value).to be_nil
+      end
+
+      it "raises a ShutdownError if a the pool is stopped before requesting a store" do
+        subject.stop
+        expect{ subject.check_out }.to raise_error Moneta::Pool::ShutdownError
+      end
+    end
+  end
 end
