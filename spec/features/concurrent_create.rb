15345ecd3035319d3f1c60b39a180e378ab020d0
Alastair Pharo <me@asph.dev>
2019-09-15 14:51:30 +1000

    Specs: rewrite concurrent create/increment specs
    Attempts to make it clearer what is being tested for

--- spec/features/concurrent_create.rb
+++ spec/features/concurrent_create.rb
@@ -1,21 +1,42 @@
 shared_examples :concurrent_create do
+  # Each thread attempts to create
   def create_thread(name)
     Thread.new do
       s = new_store
-      1000.times do |i|
-        s[i.to_s].should == name if s.create(i.to_s, name, expires: false)
-        Thread.pass if rand(100) >= 99
+      begin
+        (0...1000).map do |i|
+          s.create(i.to_s, name, expires: false).tap do
+            Thread.pass if rand(100) >= 99
+          end
+        end
+      ensure
+        s.close
       end
-      s.close
     end
   end
 
   it 'have atomic create across multiple threads', isolate: true do
-    a = create_thread('a')
-    b = create_thread('b')
-    c = create_thread('c')
-    a.join
-    b.join
-    c.join
+    names = %w{a b c}
+
+    # Spawn threads and then group results (lists of true/false values) by
+    # store index (0...1000)
+    results = names
+      .map { |name| create_thread(name) }
+      .map(&:value)
+      .transpose.each_with_index
+      .map { |created_values, i| [i.to_s, created_values] }
+      .to_h
+
+    # Just a quick sanity check
+    expect(results.length).to eq 1000
+
+    # Ensure that for each index, one and only one created value is true
+    expect(results.map { |_, created_values| created_values.inject(:^) }).to all(be true)
+
+    # Check that the when a call to create returned true, that the store
+    # contains the correct value as a result
+    expect(store.slice(*results.keys).to_h).to eq(results.map do |i, values|
+      [i, names[values.index(true)]]
+    end.to_h)
   end
 end
--- spec/features/concurrent_increment.rb
+++ spec/features/concurrent_increment.rb
@@ -2,31 +2,39 @@ shared_examples :concurrent_increment do
   def increment_thread(name)
     Thread.new do
       s = new_store
-      100.times do |i|
-        100.times do |j|
-          s.increment("counter#{j}", 1, expires: false)
-          Thread.pass if rand(1000) >= 995
+      begin
+        # Create an array where each entry is a list of all the return values
+        # from calling increment for a particular key.
+        increments = (0...100).map { [] }
+        100.times do
+          100.times do |j|
+            increments[j] << s.increment(j.to_s, 1, expires: false).tap do
+              Thread.pass if rand(1000) >= 995
+            end
+          end
         end
-        s.store("#{name}#{i}", i.to_s, expires: false)
+        increments
+      ensure
+        s.close
       end
-      s.close
     end
   end
 
   it 'have atomic increment across multiple threads', isolate: true do
-    a = increment_thread('a')
-    b = increment_thread('b')
-    c = increment_thread('c')
-    a.join
-    b.join
-    c.join
-    100.times do |i|
-      store["a#{i}"].should == i.to_s
-      store["b#{i}"].should == i.to_s
-      store["c#{i}"].should == i.to_s
-    end
-    100.times do |j|
-      store.raw["counter#{j}"].should == 300.to_s
+    results = %w{a b c}
+      .map { |name| increment_thread(name) }
+      .map(&:value)
+      .transpose # Now the array is indexed by store key instead of thread
+
+    # Sanity check
+    expect(results.length).to eq 100
+
+    results.each do |ith_values|
+      # ensure that for each pair in the triple there are no overlapping values
+      expect(ith_values.combination(2).map { |a, b| a & b }).to all be_empty
+
+      # ensure that when joined together they cover the full 1..300 range
+      expect(ith_values.inject(:+)).to contain_exactly(*1..300)
     end
   end
 end
