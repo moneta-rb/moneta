066651b4792230b5ceb985a61303316fcd766562
Alastair Pharo <me@asph.dev>
2020-02-23 19:50:51 +1100

    Travis: skip kyotocabinet on 2.7
    kyotocabinet-ruby-reanimated doesn't seem to build on Ruby 2.7.

--- spec/moneta/adapters/kyotocabinet/adapter_kyotocabinet_spec.rb
+++ spec/moneta/adapters/kyotocabinet/adapter_kyotocabinet_spec.rb
@@ -1,4 +1,4 @@
-describe 'adapter_kyotocabinet', unsupported: defined?(JRUBY_VERSION), adapter: :KyotoCabinet do
+describe 'adapter_kyotocabinet', unsupported: defined?(JRUBY_VERSION) || ::Gem::Version.new(RUBY_ENGINE_VERSION) >= ::Gem::Version.new('2.7.0'), adapter: :KyotoCabinet do
   moneta_build do
     Moneta::Adapters::KyotoCabinet.new(file: File.join(tempdir, "adapter_kyotocabinet.kch"))
   end
--- spec/moneta/adapters/kyotocabinet/standard_kyotocabinet_with_expires_spec.rb
+++ spec/moneta/adapters/kyotocabinet/standard_kyotocabinet_with_expires_spec.rb
@@ -1,4 +1,4 @@
-describe 'standard_kyotocabinet_with_expires', unsupported: defined?(JRUBY_VERSION), adapter: :KyotoCabinet do
+describe 'standard_kyotocabinet_with_expires', unsupported: defined?(JRUBY_VERSION)|| ::Gem::Version.new(RUBY_ENGINE_VERSION) >= ::Gem::Version.new('2.7.0'), adapter: :KyotoCabinet do
   let(:t_res) { 0.125 }
   let(:min_ttl) { t_res }
   use_timecop
