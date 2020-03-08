1b02c98f3e2a6df3e798d7cb576da460359e82e9
Mauro Asprea <mauroasprea@gmail.com>
2020-03-08 16:46:54 +0100

    Fix tcp and unix client specs

--- spec/moneta/adapters/client/standard_client_unix_spec.rb
+++ spec/moneta/adapters/client/standard_client_unix_spec.rb
@@ -9,5 +9,20 @@ describe "standard_client_unix", adapter: :Client do
     { socket: File.join(tempdir, 'standard_client_unix') }
   end
 
-  moneta_specs STANDARD_SPECS
+  moneta_specs STANDARD_SPECS.with_each_key
+
+  it 'supports multiple clients' do
+    store['shared_key'] = 'shared_val'
+    threads = (1..32).map do |i|
+      Thread.new do
+        client = new_store
+        (1..31).each do |j|
+          client['shared_key'].should == 'shared_val'
+          client["key-\#{j}-\#{i}"] = "val-\#{j}-\#{i}"
+          client["key-\#{j}-\#{i}"].should == "val-\#{j}-\#{i}"
+        end
+      end
+    end
+    threads.map(&:join)
+  end
 end
