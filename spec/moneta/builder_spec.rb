8688ffa583cb6161b50813f56bac4bbf1ec97187
Alastair Pharo <me@asph.dev>
2020-04-13 15:48:51 +1000

    Builder: dup options before instantiating adapters/proxies
    Fixes #173

--- spec/moneta/builder_spec.rb
+++ spec/moneta/builder_spec.rb
@@ -25,4 +25,26 @@ describe Moneta::Builder do
       end.build
     end.to raise_error /Please check/
   end
+
+  it 'dups options before passing them to each middleware' do
+    my_adapter = Class.new do
+      def initialize(options)
+        throw "a is missing" unless options.delete(:a)
+      end
+    end
+
+    my_middleware = Class.new do
+      def initialize(backend, options)
+        throw "a is missing" unless options.delete(:a)
+      end
+    end
+
+    options = { a: 1 }
+    Moneta::Builder.new do
+      use my_middleware, options
+      adapter my_adapter, options
+    end.build
+
+    expect(options).to include(a: 1)
+  end
 end
