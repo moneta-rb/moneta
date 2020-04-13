09abea097629bf8d0b0b836bb2ba6a4fa77d9416
Mauro Asprea <mauroasprea@gmail.com>
2020-04-13 20:31:52 +0200

    Add spec for a shared backend with other prefixes

--- spec/moneta/adapters/memory/standard_memory_with_prefix_spec.rb
+++ spec/moneta/adapters/memory/standard_memory_with_prefix_spec.rb
@@ -1,8 +1,8 @@
 describe 'standard_memory_with_prefix', adapter: :Memory do
-  moneta_store :Memory, {prefix: "moneta"}
+  moneta_store :Memory, { prefix: "moneta" }
   moneta_specs STANDARD_SPECS.without_persist.with_each_key
 
-  context 'with keys with no prefix' do
+  context 'with keys from no prefix' do
     before(:each) do
       store.adapter.adapter.backend['no_prefix'] = 'hidden'
     end
@@ -13,4 +13,29 @@ describe 'standard_memory_with_prefix', adapter: :Memory do
 
     include_examples :each_key
   end
+
+  context 'with keys from other prefixes' do
+    before do
+      backend = store.adapter.adapter.backend
+      @alternative_store ||= Moneta.build do
+        use :Transformer, key: [:marshal, :prefix], value: :marshal, prefix: 'alternative_'
+        adapter :Memory, backend: backend
+      end
+      expect(@alternative_store).to be_a(Moneta::Transformer::MarshalPrefixKeyMarshalValue)
+    end
+    let(:alternative) { @alternative_store }
+
+    before(:each) do
+      alternative.store('with_prefix_key', 'hidden')
+    end
+
+    after(:each) do
+      expect(store.adapter.adapter.backend.keys).to include('alternative_with_prefix_key')
+      expect(alternative.each_key.to_a).to eq(['with_prefix_key'])
+      expect(alternative['with_prefix_key']).to eq('hidden')
+    end
+
+    include_examples :each_key
+  end
+
 end
