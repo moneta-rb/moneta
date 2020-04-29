bc6374ecb0536dbd6ac468743170ff4f2079acff
Alastair Pharo <me@asph.dev>
2020-04-29 18:10:29 +1000

    Specs: don't rerun expiry tests with different usecs
    Fixes #181

--- spec/features/default_expires.rb
+++ spec/features/default_expires.rb
@@ -1,15 +1,14 @@
 shared_examples :default_expires do
-  at_each_usec do
-    it 'sets the default expiration time', default_expires: true do
-      store['key1'] = 'val1'
-      advance(t_res / 4.0) # sleep less than a single time-space
-      store.key?('key1').should be true
-      store.fetch('key1').should == 'val1'
-      store.load('key1').should == 'val1'
-      advance min_ttl + t_res
-      store.key?('key1').should be false
-      store.fetch('key1').should be_nil
-      store.load('key1').should be_nil
-    end
+  it 'sets the default expiration time', default_expires: true do
+    store['key1'] = 'val1'
+    advance(t_res / 4.0) # sleep less than a single time-space
+    store.key?('key1').should be true
+    store.fetch('key1').should == 'val1'
+    store.load('key1').should == 'val1'
+    advance min_ttl
+    2.times { advance_next_tick }
+    store.key?('key1').should be false
+    store.fetch('key1').should be_nil
+    store.load('key1').should be_nil
   end
 end
