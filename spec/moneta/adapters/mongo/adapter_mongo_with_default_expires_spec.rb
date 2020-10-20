8327cedceb2ce38ea2498fee36223b2dc3a4b6f4
Alastair Pharo <me@asph.dev>
2020-10-20 12:31:11 +1100

    Mongo: document :database option and deprecate :db option

--- spec/moneta/adapters/mongo/adapter_mongo_with_default_expires_spec.rb
+++ spec/moneta/adapters/mongo/adapter_mongo_with_default_expires_spec.rb
@@ -3,11 +3,11 @@ describe 'adapter_mongo_with_default_expires', isolate: true, adapter: :Mongo do
   let(:min_ttl) { t_res }
 
   moneta_build do
-    Moneta::Adapters::Mongo.new(mongo_config(
-    	db: File.basename(__FILE__, '.rb'),
+    Moneta::Adapters::Mongo.new(
+    	database: File.basename(__FILE__, '.rb'),
       collection: 'adapter_mongo_with_default_expires',
       expires: min_ttl
-    ))
+    )
   end
 
   moneta_specs ADAPTER_SPECS.with_each_key.with_expires.with_default_expires.simplevalues_only
