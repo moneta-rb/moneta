e3fa7314e06a8eda61c7c34cf32320a568575c50
Alastair Pharo <me@asph.dev>
2021-03-08 21:59:52 +1100

    Specs: increase min TTL for Mongo

--- spec/moneta/adapters/mongo/adapter_mongo_with_default_expires_spec.rb
+++ spec/moneta/adapters/mongo/adapter_mongo_with_default_expires_spec.rb
@@ -1,6 +1,6 @@
 describe 'adapter_mongo_with_default_expires', isolate: true, adapter: :Mongo do
   let(:t_res) { 0.125 }
-  let(:min_ttl) { t_res }
+  let(:min_ttl) { t_res * 4 }
 
   moneta_build do
     Moneta::Adapters::Mongo.new(
