f3507cbb8472cf7948673e144431bd1ed7854457
Alastair Pharo <me@asph.dev>
2021-06-12 14:40:05 +1000

    MemcachedDalli: inherit Adapter

--- lib/moneta/adapters/memcached/dalli.rb
+++ lib/moneta/adapters/memcached/dalli.rb
@@ -4,12 +4,14 @@ module Moneta
   module Adapters
     # Memcached backend (using gem dalli)
     # @api public
-    class MemcachedDalli
-      include Defaults
+    class MemcachedDalli < Adapter
       include ExpiresSupport
 
       supports :create, :increment
-      attr_reader :backend
+
+      config :expires
+
+      backend { |server: '127.0.0.1:11211', **options| ::Dalli::Client.new(server, options) }
 
       # @param [Hash] options
       # @option options [String] :server ('127.0.0.1:11211') Memcached server
@@ -17,34 +19,30 @@ module Moneta
       # @option options [::Dalli::Client] :backend Use existing backend instance
       # @option options Other options passed to `Dalli::Client#new`
       def initialize(options = {})
-        self.default_expires = options.delete(:expires)
-        @backend = options[:backend] ||
-          begin
-            server = options.delete(:server) || '127.0.0.1:11211'
-            ::Dalli::Client.new(server, options)
-          end
+        super
+        self.default_expires = config.expires
       end
 
       # (see Proxy#load)
       def load(key, options = {})
-        value = @backend.get(key)
+        value = backend.get(key)
         if value
           expires = expires_value(options, nil)
-          @backend.set(key, value, expires || nil, raw: true) if expires != nil
+          backend.set(key, value, expires || nil, raw: true) if expires != nil
           value
         end
       end
 
       # (see Proxy#store)
       def store(key, value, options = {})
-        @backend.set(key, value, expires_value(options) || nil, raw: true)
+        backend.set(key, value, expires_value(options) || nil, raw: true)
         value
       end
 
       # (see Proxy#delete)
       def delete(key, options = {})
-        value = @backend.get(key)
-        @backend.delete(key)
+        value = backend.get(key)
+        backend.delete(key)
         value
       end
 
@@ -52,9 +50,9 @@ module Moneta
       def increment(key, amount = 1, options = {})
         result =
           if amount >= 0
-            @backend.incr(key, amount, expires_value(options) || nil)
+            backend.incr(key, amount, expires_value(options) || nil)
           else
-            @backend.decr(key, -amount, expires_value(options) || nil)
+            backend.decr(key, -amount, expires_value(options) || nil)
           end
         if result
           result
@@ -67,32 +65,32 @@ module Moneta
 
       # (see Proxy#clear)
       def clear(options = {})
-        @backend.flush_all
+        backend.flush_all
         self
       end
 
       # (see Defaults#create)
       def create(key, value, options = {})
-        !!@backend.add(key, value, expires_value(options) || nil, raw: true)
+        !!backend.add(key, value, expires_value(options) || nil, raw: true)
       end
 
       # (see Proxy#close)
       def close
-        @backend.close
+        backend.close
         nil
       end
 
       # (see Defaults#slice)
       def slice(*keys, **options)
-        @backend.get_multi(keys).tap do |pairs|
+        backend.get_multi(keys).tap do |pairs|
           next if pairs.empty?
           expires = expires_value(options, nil)
           next if expires == nil
           expires = expires.to_i if Numeric === expires
           expires ||= 0
-          @backend.multi do
+          backend.multi do
             pairs.each do |key, value|
-              @backend.set(key, value, expires, false)
+              backend.set(key, value, expires, false)
             end
           end
         end
@@ -112,7 +110,7 @@ module Moneta
 
         if block_given?
           keys = pairs.map { |key, _| key }.to_a
-          old_pairs = @backend.get_multi(keys)
+          old_pairs = backend.get_multi(keys)
           pairs = pairs.map do |key, new_value|
             if old_pairs.key? key
               new_value = yield(key, old_pairs[key], new_value)
@@ -121,9 +119,9 @@ module Moneta
           end
         end
 
-        @backend.multi do
+        backend.multi do
           pairs.each do |key, value|
-            @backend.set(key, value, expires, raw: true)
+            backend.set(key, value, expires, raw: true)
           end
         end
 
