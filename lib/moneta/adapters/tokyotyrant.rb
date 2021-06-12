c6b813215591f6c682fca86d8a372afa2df5be67
Alastair Pharo <me@asph.dev>
2021-06-12 14:51:01 +1000

    TokyoTyrant: inherit Adapter

--- lib/moneta/adapters/tokyotyrant.rb
+++ lib/moneta/adapters/tokyotyrant.rb
@@ -10,51 +10,50 @@ module Moneta
   module Adapters
     # TokyoTyrant backend
     # @api public
-    class TokyoTyrant
-      include Defaults
+    class TokyoTyrant < Adapter
       include HashAdapter
 
       # error code: no record found
       ENOREC = 7
 
       supports :create, :increment
-      attr_reader :backend
+
+      backend do |host: '127.0.0.1', port: 1978|
+        if defined?(::TokyoTyrant::RDB)
+          # Use ruby client
+          ::TokyoTyrant::RDB.new.tap do |backend|
+            backend.open(host, port) or raise backend.errmsg
+          end
+        else
+          # Use native client
+          ::TokyoTyrant::DB.new(host, port)
+        end
+      end
 
       # @param [Hash] options
       # @option options [String] :host ('127.0.0.1') Server host name
       # @option options [Integer] :port (1978) Server port
       # @option options [::TokyoTyrant::RDB] :backend Use existing backend instance
       def initialize(options = {})
-        options[:host] ||= '127.0.0.1'
-        options[:port] ||= 1978
-        if options[:backend]
-          @backend = options[:backend]
-        elsif defined?(::TokyoTyrant::RDB)
-          # Use ruby client
-          @backend = ::TokyoTyrant::RDB.new
-          @backend.open(options[:host], options[:port]) or error
-        else
-          # Use native client
-          @backend = ::TokyoTyrant::DB.new(options[:host], options[:port])
-        end
-        @native = @backend.class.name != 'TokyoTyrant::RDB'
+        super
+        @native = backend.class.name != 'TokyoTyrant::RDB'
         probe = '__tokyotyrant_endianness_probe'
-        @backend.delete(probe)
-        @backend.addint(probe, 1)
-        @pack = @backend.delete(probe) == [1].pack('l>') ? 'l>' : 'l<'
+        backend.delete(probe)
+        backend.addint(probe, 1)
+        @pack = backend.delete(probe) == [1].pack('l>') ? 'l>' : 'l<'
       end
 
       # (see Proxy#load)
       def load(key, options = {})
-        value = @backend[key]
+        value = backend[key]
         # raise if there is an error and the error is not "no record"
-        error if value == nil && @backend.ecode != ENOREC
+        error if value == nil && backend.ecode != ENOREC
         value && unpack(value)
       end
 
       # (see Proxy#store)
       def store(key, value, options = {})
-        @backend.put(key, pack(value)) or error
+        backend.put(key, pack(value)) or error
         value
       end
 
@@ -62,14 +61,14 @@ module Moneta
       def delete(key, options = {})
         value = load(key, options)
         if value
-          @backend.delete(key) or error
+          backend.delete(key) or error
           value
         end
       end
 
       # (see Proxy#increment)
       def increment(key, amount = 1, options = {})
-        @backend.addint(key, amount) or error
+        backend.addint(key, amount) or error
       end
 
       # (see Proxy#create)
@@ -77,18 +76,18 @@ module Moneta
         if @native
           begin
             # Native client throws an exception
-            @backend.putkeep(key, pack(value))
+            backend.putkeep(key, pack(value))
           rescue TokyoTyrantError
             false
           end
         else
-          @backend.putkeep(key, pack(value))
+          backend.putkeep(key, pack(value))
         end
       end
 
       # (see Proxy#close)
       def close
-        @backend.close
+        backend.close
         nil
       end
 
@@ -96,10 +95,10 @@ module Moneta
       def slice(*keys, **options)
         hash =
           if @native
-            @backend.mget(*keys)
+            backend.mget(*keys)
           else
             hash = Hash[keys.map { |key| [key] }]
-            raise unless @backend.mget(hash) >= 0
+            raise unless backend.mget(hash) >= 0
             hash
           end
 
@@ -142,7 +141,7 @@ module Moneta
       end
 
       def error
-        raise "#{@backend.class.name} error: #{@backend.errmsg}"
+        raise "#{backend.class.name} error: #{backend.errmsg}"
       end
     end
   end
