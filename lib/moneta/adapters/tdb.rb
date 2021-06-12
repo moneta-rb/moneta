d596f81851e91ef6178aa44b1056895095e57fdd
Alastair Pharo <me@asph.dev>
2021-06-12 14:48:50 +1000

    TDB: inherit Adapter

--- lib/moneta/adapters/tdb.rb
+++ lib/moneta/adapters/tdb.rb
@@ -4,34 +4,28 @@ module Moneta
   module Adapters
     # TDB backend
     # @api public
-    class TDB
-      include Defaults
+    class TDB < Adapter
       include HashAdapter
       include IncrementSupport
       include EachKeySupport
 
       supports :create
 
-      # @param [Hash] options
-      # @option options [String] :file Database file
-      # @option options [::TDB] :backend Use existing backend instance
-      def initialize(options)
-        @backend = options[:backend] ||
-          begin
-            raise ArgumentError, 'Option :file is required' unless file = options.delete(:file)
-            ::TDB.new(file, options)
-          end
-      end
+      # @!method initialize(options = {})
+      #   @param [Hash] options
+      #   @option options [String] :file Database file
+      #   @option options [::TDB] :backend Use existing backend instance
+      backend { |file:, **options| ::TDB.new(file, options) }
 
       # (see Proxy#close)
       def close
-        @backend.close
+        backend.close
         nil
       end
 
       # (see Proxy#create)
       def create(key, value, options = {})
-        @backend.insert!(key, value)
+        backend.insert!(key, value)
         true
       rescue ::TDB::ERR::EXISTS
         false
