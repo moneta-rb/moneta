ecdf0be3ae1844aa965a0ff51b7170600912cbf6
Alastair Pharo <me@asph.dev>
2021-06-12 14:31:48 +1000

    DBM: inherit Adapter

--- lib/moneta/adapters/dbm.rb
+++ lib/moneta/adapters/dbm.rb
@@ -4,23 +4,17 @@ module Moneta
   module Adapters
     # DBM backend (Berkeley DB)
     # @api public
-    class DBM
-      include Defaults
+    class DBM < Adapter
       include DBMAdapter
       include IncrementSupport
       include CreateSupport
       include EachKeySupport
 
-      # @param [Hash] options
-      # @option options [String] :file Database file
-      # @option options [::DBM] :backend Use existing backend instance
-      def initialize(options = {})
-        @backend = options[:backend] ||
-          begin
-            raise ArgumentError, 'Option :file is required' unless options[:file]
-            ::DBM.new(options[:file])
-          end
-      end
+      # @!method initialize(options = {})
+      #   @param [Hash] options
+      #   @option options [String] :file Database file
+      #   @option options [::DBM] :backend Use existing backend instance
+      backend { |file:| ::DBM.new(file) }
     end
   end
 end
