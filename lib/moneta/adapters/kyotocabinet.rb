a70763fe80a3261703a1b7592024f004553978cc
Alastair Pharo <me@asph.dev>
2021-06-12 14:36:19 +1000

    KyotoCabinet: inherit Adapter

--- lib/moneta/adapters/kyotocabinet.rb
+++ lib/moneta/adapters/kyotocabinet.rb
@@ -4,58 +4,53 @@ module Moneta
   module Adapters
     # KyotoCabinet backend
     # @api public
-    class KyotoCabinet
-      include Defaults
+    class KyotoCabinet < Adapter
       include HashAdapter
 
       supports :each_key, :increment, :create
 
-      # @param [Hash] options
-      # @option options [String] :file Database file
-      # @option options [::KyotoCabinet::DB] :backend Use existing backend instance
-      def initialize(options = {})
-        if options[:backend]
-          @backend = options[:backend]
-        else
-          raise ArgumentError, 'Option :file is required' unless options[:file]
-          @backend = ::KyotoCabinet::DB.new
-          raise @backend.error.to_s unless @backend.open(options[:file],
-                                                         ::KyotoCabinet::DB::OWRITER | ::KyotoCabinet::DB::OCREATE)
-        end
+      # @!method initialize(options = {})
+      #   @param [Hash] options
+      #   @option options [String] :file Database file
+      #   @option options [::KyotoCabinet::DB] :backend Use existing backend instance
+      backend do |file:|
+        backend = ::KyotoCabinet::DB.new
+        raise backend.error.to_s unless backend.open(file, ::KyotoCabinet::DB::OWRITER | ::KyotoCabinet::DB::OCREATE)
+        backend
       end
 
       # (see Proxy#key?)
       def key?(key, options = {})
-        @backend.check(key) >= 0
+        backend.check(key) >= 0
       end
 
       # (see Proxy#delete)
       def delete(key, options = {})
-        @backend.seize(key)
+        backend.seize(key)
       end
 
       # (see Proxy#create)
       def create(key, value, options = {})
-        @backend.add(key, value)
+        backend.add(key, value)
       end
 
       # (see Proxy#close)
       def close
-        @backend.close
+        backend.close
         nil
       end
 
       # (see Proxy#each_key)
       def each_key
-        return enum_for(:each_key) { @backend.count } unless block_given?
-        @backend.each_key { |arr| yield arr[0] }
+        return enum_for(:each_key) { backend.count } unless block_given?
+        backend.each_key { |arr| yield arr[0] }
         self
       end
 
       # (see Proxy#increment)
       def increment(key, amount = 1, options = {})
         ret = nil
-        success = @backend.accept(key) do |_, value|
+        success = backend.accept(key) do |_, value|
           ret =
             if value
               Integer(value) + amount
@@ -65,13 +60,13 @@ module Moneta
           ret.to_s
         end
 
-        raise @backend.error unless success
+        raise backend.error unless success
         ret
       end
 
       # (see Proxy#slice)
       def slice(*keys, atomic: true, **options)
-        @backend.get_bulk(keys, atomic)
+        backend.get_bulk(keys, atomic)
       end
 
       # (see Proxy#values_at)
@@ -87,7 +82,7 @@ module Moneta
 
         success =
           if block_given?
-            @backend.transaction(hard) do
+            backend.transaction(hard) do
               existing = slice(*pairs.map { |k, _| k }, **options)
               pairs = pairs.map do |key, new_value|
                 if existing.key?(key)
@@ -96,13 +91,13 @@ module Moneta
                   [key, new_value]
                 end
               end
-              @backend.set_bulk(pairs.to_h, atomic) >= 0
+              backend.set_bulk(pairs.to_h, atomic) >= 0
             end
           else
-            @backend.set_bulk(pairs.to_h, atomic) >= 0
+            backend.set_bulk(pairs.to_h, atomic) >= 0
           end
 
-        raise @backend.error unless success
+        raise backend.error unless success
         self
       end
     end
