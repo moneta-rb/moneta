65869f5d101fe4fa8991e7ab2b291e5ab90f2857
Alastair Pharo <me@asph.dev>
2021-11-27 15:20:02 +1100

    Specs: use Webrick for REST server on JRuby too

--- spec/moneta/adapters/faraday_helper.rb
+++ spec/moneta/adapters/faraday_helper.rb
@@ -1,9 +1,10 @@
 RSpec.shared_context :faraday_adapter do
   before :context do
-    require 'faraday/adapter/manticore' if defined?(JRUBY_VERSION)
+    #require 'faraday/adapter/manticore' if defined?(JRUBY_VERSION)
   end
 
   let(:faraday_adapter) do
-    defined?(JRUBY_VERSION) ? :manticore : :net_http
+    #defined?(JRUBY_VERSION) ? :manticore : :net_http
+    :net_http
   end
 end
