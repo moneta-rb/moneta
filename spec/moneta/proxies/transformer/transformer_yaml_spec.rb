e4d1de74664ca02024f3b4c3026183f1d98a8263
Alastair Pharo <me@asph.dev>
2022-12-10 13:27:44 +1100

    Specs: mark yaml transformer as broken on Ruby 3.1+

--- spec/moneta/proxies/transformer/transformer_yaml_spec.rb
+++ spec/moneta/proxies/transformer/transformer_yaml_spec.rb
@@ -1,4 +1,4 @@
-describe 'transformer_yaml', proxy: :Transformer do
+describe 'transformer_yaml', proxy: :Transformer, broken: ::Gem::Version.new(RUBY_VERSION) >= ::Gem::Version.new('3.1.0') do
   moneta_build do
     Moneta.build do
       use :Transformer, key: :yaml, value: :yaml
