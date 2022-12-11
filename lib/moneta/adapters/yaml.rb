7ee3c26f53f53934e6ab30eaed8452facc6b75cd
Alastair Pharo <me@asph.dev>
2022-12-11 11:48:44 +1100

    Adapters::YAML - allow passing options to YAML::Store

--- lib/moneta/adapters/yaml.rb
+++ lib/moneta/adapters/yaml.rb
@@ -5,7 +5,13 @@ module Moneta
     # YAML::Store backend
     # @api public
     class YAML < PStore
-      backend { |file:| ::YAML::Store.new(file) }
+      # @!method initialize(options = {})
+      #   @param [Hash] options
+      #   @option options [YAML::Store] :backend YAML store to use
+      #   @option options [String] :file YAML file to open - required unless using :backend
+      #   @option options [Boolean] :threadsafe (false) Makes the YAML store thread-safe
+      #   @option options Other options passed to `YAML::Store#new`
+      backend { |file:, threadsafe: false, **options| ::YAML::Store.new(file, threadsafe, options) }
     end
   end
 end
