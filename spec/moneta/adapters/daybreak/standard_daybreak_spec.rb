179db21a38b25b63e241a6434a038d417c6d156a
Alastair Pharo <me@asph.dev>
2023-03-12 19:13:25 +1100

    Tests: mark BERT and Daybreak broken on 3.2

--- spec/moneta/adapters/daybreak/standard_daybreak_spec.rb
+++ spec/moneta/adapters/daybreak/standard_daybreak_spec.rb
@@ -1,4 +1,4 @@
-describe 'standard_daybreak', adapter: :Daybreak do
+describe 'standard_daybreak', broken: ::Gem::Version.new(RUBY_VERSION) >= ::Gem::Version.new('3.2.0'), adapter: :Daybreak do
   moneta_store :Daybreak do
     {file: File.join(tempdir, "simple_daybreak")}
   end
