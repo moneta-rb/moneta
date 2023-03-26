985faa81cac345ac37d2edee9ddb450dc20d066f
Alastair Pharo <me@asph.dev>
2023-03-26 18:29:43 +0800

    Transforms::Truncate: fix initializer

--- lib/moneta/transforms/truncate.rb
+++ lib/moneta/transforms/truncate.rb
@@ -3,14 +3,14 @@ require 'digest'
 module Moneta
   module Transforms
     class Truncate < Transform
-      def initialize(maxlen: 32)
+      def initialize(maxlen: 32, **_)
         super
         @maxlen = maxlen
       end
 
       def encode(value)
         if value.size >= @maxlen
-          digest = Digest::MD5.hexdigest(value)
+          digest = ::Digest::MD5.hexdigest(value)
           value[0, @maxlen - digest.size] << digest
         else
           value
