24f39e5038756b63a7d90bc05fded7f3e2511f14
Alastair Pharo <me@asph.dev>
2023-03-26 09:35:53 +0800

    Specs: modify each_key+prefix checks & move to a different location

--- spec/moneta/adapters/memory/standard_memory_with_prefix_spec.rb
+++ spec/moneta/adapters/memory/standard_memory_with_prefix_spec.rb
@@ -1,40 +1,4 @@
 describe 'standard_memory_with_prefix', adapter: :Memory do
   moneta_store :Memory, { prefix: "moneta" }
   moneta_specs STANDARD_SPECS.without_persist.with_each_key
-
-  context 'with keys from no prefix' do
-    before(:each) do
-      store.adapter.adapter.backend['no_prefix'] = 'hidden'
-    end
-
-    after(:each) do
-      expect(store.adapter.adapter.backend.keys).to include('no_prefix')
-    end
-
-    include_examples :each_key
-  end
-
-  context 'with keys from other prefixes' do
-    before do
-      backend = store.adapter.adapter.backend
-      @alternative_store ||= Moneta.build do
-        use :Transformer, key: [:marshal, :prefix], value: :marshal, prefix: 'alternative_'
-        adapter :Memory, backend: backend
-      end
-    end
-    let(:alternative) { @alternative_store }
-
-    before(:each) do
-      alternative.store('with_prefix_key', 'hidden')
-    end
-
-    after(:each) do
-      expect(store.adapter.adapter.backend.keys).to include('alternative_with_prefix_key')
-      expect(alternative.each_key.to_a).to eq(['with_prefix_key'])
-      expect(alternative['with_prefix_key']).to eq('hidden')
-    end
-
-    include_examples :each_key
-  end
-
 end
--- spec/moneta/proxies/transformer/transformer_marshal_prefix_base64_spec.rb
+++ spec/moneta/proxies/transformer/transformer_marshal_prefix_base64_spec.rb
@@ -1,6 +1,5 @@
 describe 'transformer_marshal_prefix_base64', proxy: :Transformer do
   moneta_build do
-
     Moneta.build do
       use :Transformer, key: [:marshal, :prefix, :base64], value: [:marshal, :base64], prefix: 'moneta'
       adapter :Memory
@@ -13,16 +12,35 @@ describe 'transformer_marshal_prefix_base64', proxy: :Transformer do
 
   moneta_specs STANDARD_SPECS.without_persist.with_each_key
 
-  context 'with keys with no prefix' do
-    before(:each) do
-      store.adapter.backend['no_prefix'] = 'hidden'
+  context 'sharing the backend with a store without the prefix' do
+    let :other_store do
+      ::Moneta::Adapters::Memory.new(backend: store.adapter.backend)
     end
 
-    after(:each) do
-      expect(store.adapter.backend.keys).to include('no_prefix')
+    it "doesn't include unprefixed keys in calls to #each_key" do
+      store['x'] = 1
+      other_store['x'] = 2
+      expect { |b| other_store.each_key(&b) }.to yield_successive_args(a_string_starting_with('bW9uZXRh'), 'x')
+      expect { |b| store.each_key(&b) }.to yield_with_args('x')
     end
-
-    include_examples :each_key
   end
 
+  context 'sharing the backend with a store with a distinct prefix' do
+    let(:backend) { store.adapter.backend }
+    let :other_store do
+      backend = self.backend
+      Moneta.build do
+        use :Transformer, key: [:marshal, :prefix], value: :marshal, prefix: 'alternative'
+        adapter :Memory, backend: backend
+      end
+    end
+
+    it "is not possible for either store to see the other's keys" do
+      store['x'] = 1
+      other_store['y'] = 2
+      expect { |b| backend.each_key(&b) }.to yield_successive_args(a_string_starting_with('bW9uZXRh'), a_string_starting_with('alternative'))
+      expect { |b| store.each_key(&b) }.to yield_with_args('x')
+      expect { |b| other_store.each_key(&b) }.to yield_with_args('y')
+    end
+  end
 end
