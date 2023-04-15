78c3b8b3b2db2aa878d7aede9f1d76c75a9048f0
Alastair Pharo <me@asph.dev>
2023-04-15 16:21:15 +1000

    Transforms::BEncode: raise error if deserialize returns nil
    BEncode can't encode nil values, so this can never happen

--- lib/moneta/transforms/bencode.rb
+++ lib/moneta/transforms/bencode.rb
@@ -9,7 +9,9 @@ module Moneta
 
       def deserialize(value)
         # BEncode needs a mutable string
-        ::BEncode.load(value.dup)
+        ::BEncode.load(value.dup).tap do |deserialized_value|
+          raise "::BEncode.load returned nil" if deserialized_value == nil
+        end
       end
     end
   end
